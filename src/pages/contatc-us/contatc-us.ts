502ffd1a3b4b5dfa90b6dc207b912d1046fd2528
Alaa Hassan Almishlab <almishlab8@gmail.com>
2017-12-16 02:42:53 +0300

    design contact-us page and connect it with firebase

--- src/pages/contatc-us/contatc-us.ts
+++ src/pages/contatc-us/contatc-us.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams  ,ViewController} from 'ionic-angular';
-
+import { AngularFireDatabase , AngularFireList} from 'angularfire2/database';
+import{HomePage} from '../home/home'
+import { AlertController } from 'ionic-angular';
 /**
  * Generated class for the ContatcUsPage page.
  *
@@ -14,8 +16,13 @@ import { IonicPage, NavController, NavParams  ,ViewController} from 'ionic-angul
   templateUrl: 'contatc-us.html',
 })
 export class ContatcUsPage {
+  newFeedback :AngularFireList<any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams , public viewCtrl: ViewController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams 
+    , public viewCtrl: ViewController, public db:AngularFireDatabase,
+    public alertCtrl: AlertController) {
+    this.newFeedback = db.list('/Feedback');
+
   }
 
   ionViewDidLoad() {
@@ -23,5 +30,42 @@ export class ContatcUsPage {
   }
   dismiss() {
     this.viewCtrl.dismiss();
+  }
+  sendfeedback(feedback_title,feedback_subject){
+
+    let  d = new Date();
+    let time = [d.getMonth()+1,
+                d.getDate(),
+                d.getFullYear()].join('/')+' '+
+               [d.getHours(),
+                d.getMinutes(),
+                d.getSeconds()].join(':');
+       this.newFeedback.push({
+     feedback_title:feedback_title,
+     feedback_subject:feedback_subject,
+     decs:0- Date.now(),
+     time:time
+   }).then(newPerson =>{
+    this. showAlert()
+     },error => {
+     console.log(error);
+   }
+ )
+   }
+
+   showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'شكرا لك',
+      subTitle: 'شاكرين تواصلك معنا وسنأخذ برأيك في عين الاعتبار',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            this.dismiss();
+          }
+        },
+      ]
+    });
+    alert.present();
   }
 }
